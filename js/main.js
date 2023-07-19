68bbf05482fc06836753cc6da5096d23985b3b20
Jorge <81698704+ZumoshiBlade@users.noreply.github.com>
2023-07-18 21:29:49 -0400

    vFinal

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+var typed2 = new Typed('.despedida', {
+    strings: ['¡Nos vemos! 🫰', '¡Gracias por tu visita! 💖'],
+    typeSpeed: 50,
+    backDelay: 2500,
+    loop: true,
+});
+
+
+var typed1 = new Typed('.bienvenida', {
+    strings: ['Bienvenid@ al portafolio oficial de Zushi.', 'Bienvenid@ al sitio web oficial de Jorge.'],
+    typeSpeed: 50,
+    loop: true,
+});
+
